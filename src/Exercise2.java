a4f44c0e0866198090bc6c88f77b1091c8180e36
Dercont <armando.almengor@utp.ac.pa>
2023-03-17 21:15:26 +0100

    Exercise2

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,5 +1,20 @@
 public class Exercise2 {
     public static void main(String[] args) {
+    //creating new object called "mycar"
+        Car mycar = new Car();
+    //adding new door to the car
+        mycar.newDoor();
+    }
+
+    public static class Car {
+        //properties
+        int doors = 0;
+
+        //methods
+        public void newDoor(){
+            this.doors++;
+            System.out.println(doors);
+        }
 
     }
 }
