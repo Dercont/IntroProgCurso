927e1d14d7c3e5af1dc0241db1af290556982972
Dercont <armando.almengor@utp.ac.pa>
2023-03-17 20:40:19 +0100

    Exercise1 Completed

---
+++ src/Exercise1.java
@@ -0,0 +1,10 @@
+public class Exercise1 {
+    public static void main(String[] args) {
+        sumador(1, 2, 3);
+
+    }
+    static void sumador ( int a, int b, int c){
+        System.out.println(a + b + c);
+    }
+}
+
